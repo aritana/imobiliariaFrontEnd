ef6966c058f784bc9442f7865be674e25d991c87
Beatriz Campos <beatriz.campos@dtidigital.com.br>
2021-02-24 02:38:41 -0300

    implementing edit property

--- imobiliaria-frontend/src/pages/Propertie/Expanded/index.tsx
+++ imobiliaria-frontend/src/pages/Propertie/Expanded/index.tsx
@@ -1,78 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import { RouteComponentProps } from 'react-router-dom'
-import Header from '../../../components/Header'
-import PropertieDetailsHeader from '../../../components/Header/DetailsPropertie'
-import DetailPropertie from '../../../components/propertie/expanded/Details'
-import ExpandedPropertieMenu from '../../../components/propertie/expanded/Menu'
-import Visit from '../../../components/propertie/expanded/Visit'
-import { PropertieInterface } from '../../../interfaces/propertie'
-import { getPropertieById } from '../../../services/api'
+import React, { useEffect, useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import Header from "../../../components/Header";
+import PropertieDetailsHeader from "../../../components/Header/DetailsPropertie";
+import DetailPropertie from "../../../components/propertie/expanded/Details";
+import ExpandedPropertieMenu from "../../../components/propertie/expanded/Menu";
+import Visit from "../../../components/propertie/expanded/Visit";
+import Edit from "../../../components/propertie/expanded/Edit";
+import { PropertieInterface } from "../../../interfaces/propertie";
+import { getPropertieById, updatePropertie } from "../../../services/api";
 
 const ExpandedPropertie = (props: RouteComponentProps) => {
-    const [propertie, setPropertie] = useState<PropertieInterface>(undefined!)
-    const [service, setService] = useState<'edit' | 'details' | 'visit'>('details')
+  const [propertie, setPropertie] = useState<PropertieInterface>(undefined!);
+  const [service, setService] = useState<"edit" | "details" | "visit">(
+    "details"
+  );
+  const { id }: any = props.match.params;
+  useEffect(() => {
+    async function findPropertie() {
+      const { id }: any = props.match.params;
 
-    useEffect(() => {
-        async function findPropertie() {
-            const { id }: any = props.match.params
-
-            setPropertie(await getPropertieById(id))
-        }
-
-        findPropertie()
-    }, [props.match.params, service])
-
-
-
-    function getSelectedComponent() {
-        switch (service) {
-            case 'edit':
-                return (
-                    <div></div>
-                )
-            case 'details':
-                return (
-                    <DetailPropertie
-                        history={props.history as any}
-                        propertie={propertie}
-                    />
-                )
-            case 'visit':
-                return (
-                    <Visit
-                        history={props.history as any}
-                        propertie={propertie}
-                    />
-                )
-            default:
-                return (
-                    <div />
-                )
-        }
+      setPropertie(await getPropertieById(id));
     }
 
+    findPropertie();
+  }, [props.match.params, service]);
 
-    return (
-        <div className='container'>
-            <Header
-                Component={PropertieDetailsHeader}
-                componentProps={{ history: props.history }}
-            />
-            <div className='details-container'>
+  async function handleUpdateSubmit(propertieUpdated: PropertieInterface) {
+    updatePropertie(id, propertieUpdated);
+  }
 
-                <ExpandedPropertieMenu
-                    setService={setService}
-                    service={service}
-                    propertie={propertie}
-                />
-                <div className='selected-service'>
-                    {
-                        getSelectedComponent()
-                    }
-                </div>
-            </div>
-        </div>
-    )
-}
+  function getSelectedComponent() {
+    switch (service) {
+      case "edit":
+        return (
+          <Edit
+            history={props.history as any}
+            propertie={propertie}
+            handleSubmit={(propertieUpdated: PropertieInterface) =>
+              handleUpdateSubmit(propertieUpdated)
+            }
+          />
+        );
+      case "details":
+        return (
+          <DetailPropertie
+            history={props.history as any}
+            propertie={propertie}
+          />
+        );
+      case "visit":
+        return <Visit history={props.history as any} propertie={propertie} />;
+      default:
+        return <div />;
+    }
+  }
 
-export default ExpandedPropertie
+  return (
+    <div className="container">
+      <Header
+        Component={PropertieDetailsHeader}
+        componentProps={{ history: props.history }}
+      />
+      <div className="details-container">
+        <ExpandedPropertieMenu
+          setService={setService}
+          service={service}
+          propertie={propertie}
+        />
+        <div className="selected-service">{getSelectedComponent()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpandedPropertie;
--- imobiliaria-frontend/src/services/api.ts
+++ imobiliaria-frontend/src/services/api.ts
@@ -1,70 +1,91 @@
-import axios from 'axios'
-import { CreatePropertieInterface, PropertieInterface, QueryPropertiesInterface } from '../interfaces/propertie'
-import { AuthResponse } from '../interfaces/user'
-import { setUserDataOnLocalStorage } from '../utils/user'
-import { clearSession } from './auth'
+import axios from "axios";
+import {
+  CreatePropertieInterface,
+  PropertieInterface,
+  QueryPropertiesInterface,
+} from "../interfaces/propertie";
+import { AuthResponse } from "../interfaces/user";
+import { setUserDataOnLocalStorage } from "../utils/user";
+import { clearSession } from "./auth";
 
-
-const baseUrl = 'http://localhost:3333'
+const baseUrl = "http://localhost:3333";
 
 export const publicApi = axios.create({
-    baseURL: baseUrl
-})
+  baseURL: baseUrl,
+});
 
 export const privateApi = axios.create({
-    baseURL: baseUrl,
-    headers: {
-        authorization: localStorage.getItem('@yahooRespostas') || 'invalid'
-    }
-})
+  baseURL: baseUrl,
+  headers: {
+    authorization: localStorage.getItem("@yahooRespostas") || "invalid",
+  },
+});
 
 privateApi.defaults.validateStatus = (status: number) => {
-    if (status >= 400) {
-        return false
-    }
+  if (status >= 400) {
+    return false;
+  }
 
-    return true
-}
+  return true;
+};
 
+export const logIn = async (
+  email: string,
+  password: string
+): Promise<AuthResponse> => {
+  clearSession();
 
-export const logIn = async (email: string, password: string): Promise<AuthResponse> => {
-    clearSession()
+  const apiResponse = await publicApi.post("/api/users/authenticate", {
+    email,
+    password,
+  });
 
-    const apiResponse = await publicApi.post('/api/users/authenticate', { email, password })
+  setUserDataOnLocalStorage(apiResponse.data);
+  privateApi.defaults.headers.authorization = apiResponse.data.token;
 
-    setUserDataOnLocalStorage(apiResponse.data)
-    privateApi.defaults.headers.authorization = apiResponse.data.token
+  return apiResponse.data;
+};
 
-    return apiResponse.data
-}
+export const createPropertie = async (
+  propertie: CreatePropertieInterface
+): Promise<PropertieInterface> => {
+  const apiResponse = await privateApi.post("/api/properties", propertie);
 
-export const createPropertie = async (propertie: CreatePropertieInterface): Promise<PropertieInterface> => {
-    const apiResponse = await privateApi.post('/api/properties', propertie)
+  return apiResponse.data;
+};
 
-    return apiResponse.data
-}
+export const listProperties = async (
+  query: QueryPropertiesInterface
+): Promise<PropertieInterface[]> => {
+  const apiResponse = await privateApi.get("/api/properties", {
+    params: query,
+  });
 
-export const listProperties = async (query: QueryPropertiesInterface): Promise<PropertieInterface[]> => {
-    const apiResponse = await privateApi.get('/api/properties', {
-        params: query,
-    })
+  return apiResponse.data;
+};
 
-    return apiResponse.data
-}
+export const getPropertieById = async (
+  id: string
+): Promise<PropertieInterface> => {
+  const apiResponse = await privateApi.get(`/api/properties/${id}`);
 
-export const getPropertieById = async (id: string): Promise<PropertieInterface> => {
-    const apiResponse = await privateApi.get(`/api/properties/${id}`)
-
-    return apiResponse.data
-}
+  return apiResponse.data;
+};
 
 export const deletePropertie = async (id: string): Promise<void> => {
-    await privateApi.delete(`/api/properties/${id}`)
-}
+  await privateApi.delete(`/api/properties/${id}`);
+};
 
 export const createVisit = async (propertieId: string, dataHora: string) => {
-    await privateApi.post(`/api/visits`, {
-        propertieId,
-        dataHora
-    })
-}
+  await privateApi.post(`/api/visits`, {
+    propertieId,
+    dataHora,
+  });
+};
+
+export const updatePropertie = async (
+  id: string,
+  propertie: CreatePropertieInterface
+): Promise<void> => {
+  await privateApi.put(`/api/properties/${id}`, propertie);
+};
